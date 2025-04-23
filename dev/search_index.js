32eb763c46bfc49544808b49e2efbd889aca2fa2
Documenter.jl <documenter@juliadocs.github.io>
2025-04-23 01:35:23 +0000

    build based on ac22f58

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Coordinates","category":"page"},{"location":"#Coordinates","page":"Home","title":"Coordinates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Coordinates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Coordinates]","category":"page"}]
+}
