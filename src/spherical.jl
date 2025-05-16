320b296e94847034dc60bdc6f909db8fda59b69e
abhro <5664668+abhro@users.noreply.github.com>
2025-05-15 21:51:35 -0400

    Add stub implementation for negating a vector in spherical coordinates

--- src/spherical.jl
+++ src/spherical.jl
@@ -9,7 +9,12 @@ struct Spherical{T<:Real,A<:Real}
     φ::A # azimuthal angle
     function Spherical(r::T, θ::A, φ::T) where {T<:Real,A<:Real}
         nonnegative(r)
+        # FIXME θ should be constrained within [0, π]
         return new{T,A}(r, mod2pi(θ), mod2pi(φ))
     end
 end
 LinearAlgebra.norm(x::Spherical) = x.r
+Base.:-(x::Spherical) = Spherical(
+    x.r,
+    acos(-cos(x.θ)),
+    sign(-x.r*sin(x.θ)*sin(x.φ)) * acos(-cos(x.φ)))
